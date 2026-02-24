6adff67f1c789c453e666b3e8996358857fce9f1
Rishikesh Pal <palrishikesh2004@gmail.com>
2026-02-24 20:35:15 +0530

    refactor axios configuration to conditionally set baseURL based on environment

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,20 +3,22 @@ import appStore from "../store";
 import { logout } from "../store/auth/authSlice";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL:
+    location.hostname === "localhost" ? "http://localhost:8000/" : "/api",
   withCredentials: true,
 });
 
 api.interceptors.response.use(
   (response) => response,
   (err) => {
-    if(err.response?.status == 401){
+    if (err.response?.status == 401) {
       appStore.dispatch(logout());
-      
     }
 
-    return Promise.reject(err.response?.data?.message || "Something went wrong");
-  }
-)
+    return Promise.reject(
+      err.response?.data?.message || "Something went wrong",
+    );
+  },
+);
 
 export default api;
