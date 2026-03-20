2fc86dc030ddd520fc96f28d1ad1aa9743e70f1f
Rishikesh Pal <palrishikesh2004@gmail.com>
2026-03-20 10:09:17 +0530

    update theme configuration for dark mode support and refactor Navbar component for mode toggling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router";
 import { logout } from "../store/auth/authSlice";
@@ -7,9 +7,11 @@ import toast from "react-hot-toast";
 import { resetFeed } from "../store/feed/feedSlice";
 import { resetRequests } from "../store/requests/requestSlice";
 import { resetConnections } from "../store/connections/connectionsSlice";
+import { Sun, Moon } from "lucide-react";
 
 const Navbar = () => {
   const dispatch = useDispatch();
+  const [mode, setMode] = useState("fantasy");
   const user = useSelector((store) => store?.auth?.user);
   console.log(user);
   const navigate = useNavigate();
@@ -26,6 +28,12 @@ const Navbar = () => {
       console.error(err.message);
       toast.error("Logout Failed");
     }
+  };
+
+  const handleModes = () => {
+    const newMode = mode === "fantasy" ? "dark" : "fantasy";
+    setMode(newMode);
+    document.documentElement.setAttribute("data-theme", newMode);
   };
 
   return (
@@ -58,26 +66,14 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="flex gap-2 ml-auto">
-          {/* <button className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />{" "}
-              </svg>
-              <span className="badge badge-xs badge-primary indicator-item"></span>
-            </div>
-          </button> */}
+          <div
+            tabIndex={0}
+            onClick={handleModes}
+            className="btn btn-ghost btn-circle"
+          >
+            {mode === "fantasy" ? <Sun /> : <Moon />}
+          </div>
+
           <div className="dropdown dropdown-end">
             <div
               tabIndex={0}
